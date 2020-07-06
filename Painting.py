1af12ff31b5eb41d0038f814e6f0a0e1a3404618
Iqra840 <54781026+Iqra840@users.noreply.github.com>
2020-07-06 21:50:46 +0500

    Update Painting.py

--- Painting.py
+++ Painting.py
@@ -4,10 +4,10 @@ import imageio
 import matplotlib.pyplot as plt
 from PIL import Image
 # load image
-im1 = imageio.imread(‘img.jpg’)
+im1 = imageio.imread(filename)
 plt.imshow(im1)
 plt.show()
-img = cv2.imread("img.jpg")
+img = cv2.imread(filename)
 
 # smoothing outline using morphology open
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (6,6))
@@ -17,7 +17,7 @@ morph = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
 result = cv2.normalize(morph,None,20,255,cv2.NORM_MINMAX)
 
 # write result to disk
-cv2.imwrite("windmill_oilpaint.jpg", result)
+cv2.imwrite(newfile, result)
 
 cv2.imshow("IMAGE", img)
 cv2.imshow("OPEN", morph)
